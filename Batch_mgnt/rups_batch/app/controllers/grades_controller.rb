60ed686f85398cc38e643f2339f34af0969078d6
Radhika <priya.hagwane23@gmail.com>
2013-03-23 16:53:46 +0530

    made some changes in grade

--- Batch_mgnt/rups_batch/app/controllers/grades_controller.rb
+++ Batch_mgnt/rups_batch/app/controllers/grades_controller.rb
@@ -1,17 +1,17 @@
 class GradesController < ApplicationController
+def index
+	@grades=Grade.all
+	respond_to do |format|
+	format.html # index.html.erb
+	end
+
+end#index
+
+
 def new
 	@grades=Grade.new
-	#@grades = Grade.find(:all)
-
+	
 end
-
-#def create
-	#@grade=Grade.new(params[:grade])
-	#if @grade.save
-	#redirect_to new_grade_path
-#end
-#end
-
 def create
     @grades = Grade.new(params[:grade])
     respond_to do |format|
@@ -35,5 +35,28 @@ def edit
   @grades = Grade.find(params[:id])
 end
 
+def update
+    @grades = Grade.find(params[:id])
+
+    respond_to do |format|
+      if @grades.update_attributes(params[:grade])
+        format.html { redirect_to grades_path, notice: 'Grade was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @grades.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @grades = Grade.find(params[:id])
+    @grades.destroy
+
+    respond_to do |format|
+      format.html { redirect_to grades_url }
+      format.json { head :no_content }
+    end
+  end
 
 end
--- Batch_mgnt/rups_batch/app/controllers/mcities_controller.rb
+++ Batch_mgnt/rups_batch/app/controllers/mcities_controller.rb
@@ -1,14 +1,57 @@
 class McitiesController < ApplicationController
-def new
-	@mcitie=Mcitie.new
-	@mcities = Mcitie.find(:all)
+def index
+	@mcities=Mcitie.all
+	respond_to do |format|
+	format.html # index.html.erb
+	end
 
+end#index
+def create
+    @mcities = Mcitie.new(params[:mcitie])
+    respond_to do |format|
+    if @mcities.save
+    format.html {redirect_to mcitie_path(@mcities), notice: 'mcities has been created successfully' }
+    end#if
+       end#respond
+end#create
+def new
+	 @mcities = Mcitie.new
+end#new
+
+def show
+       @mcities = Mcitie.find(params[:id])
+       respond_to do |format|
+       format.html # show.html.erb
+       format.json { render json: @mcities }
+       end#do
+end#show
+def edit
+  @mcities = Mcitie.find(params[:id])
 end
 
-def create
-	@mcitie=Mcitie.new(params[:mcitie])
-	if @mcitie.save
-	redirect_to new_mcitie_path
+def update
+ @mcities = Mcitie.find(params[:id])
+
+  respond_to do |format|
+    if @mcities.update_attributes(params[:mcitie])
+      format.html  { redirect_to(@mcities,
+                    :notice => 'Record was successfully updated.') }
+      format.json  { head :no_content }
+    else
+      format.html  { render :action => "edit" }
+      format.json  { render :json => @mcities.errors,
+                    :status => :unprocessable_entity }
+    end
+  end
+end
+def destroy
+  @mcities = Mcitie.find(params[:id])
+  @mcities.destroy
+ 
+  respond_to do |format|
+    format.html { redirect_to mcities_url }
+    format.json { head :no_content }
+  
 end
 end
 end
